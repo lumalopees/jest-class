36e3b348d1b0843a063f3946c1a344ae94eb13fc
Luma <luuh_lopees@live.com>
2023-11-15 20:47:45 -0300

    settando aula

---
+++ for-Potterheads.js
@@ -0,0 +1,94 @@
+const harryPotterMovies = {
+  "filmes": [
+      {
+          "titulo": "Harry Potter e a Pedra Filosofal",
+          "ano_lancamento": 2001,
+          "atores_principais": [
+              "Daniel Radcliffe",
+              "Emma Watson",
+              "Rupert Grint",
+              "Alan Rickman",
+              "Robbie Coltrane"
+          ]
+      },
+      {
+          "titulo": "Harry Potter e a Câmara Secreta",
+          "ano_lancamento": 2002,
+          "atores_principais": [
+              "Daniel Radcliffe",
+              "Emma Watson",
+              "Rupert Grint",
+              "Alan Rickman",
+              "Kenneth Branagh"
+          ]
+      },
+      {
+          "titulo": "Harry Potter e o Prisioneiro de Azkaban",
+          "ano_lancamento": 2004,
+          "atores_principais": [
+              "Daniel Radcliffe",
+              "Emma Watson",
+              "Rupert Grint",
+              "Gary Oldman",
+              "David Thewlis"
+          ]
+      },
+      {
+          "titulo": "Harry Potter e o Cálice de Fogo",
+          "ano_lancamento": 2005,
+          "atores_principais": [
+              "Daniel Radcliffe",
+              "Emma Watson",
+              "Rupert Grint",
+              "Ralph Fiennes",
+              "Brendan Gleeson"
+          ]
+      },
+      {
+          "titulo": "Harry Potter e a Ordem da Fênix",
+          "ano_lancamento": 2007,
+          "atores_principais": [
+              "Daniel Radcliffe",
+              "Emma Watson",
+              "Rupert Grint",
+              "Helena Bonham Carter",
+              "Imelda Staunton"
+          ]
+      },
+      {
+          "titulo": "Harry Potter e o Enigma do Príncipe",
+          "ano_lancamento": 2009,
+          "atores_principais": [
+              "Daniel Radcliffe",
+              "Emma Watson",
+              "Rupert Grint",
+              "Alan Rickman",
+              "Michael Gambon"
+          ]
+      },
+      {
+          "titulo": "Harry Potter e as Relíquias da Morte - Parte 1",
+          "ano_lancamento": 2010,
+          "atores_principais": [
+              "Daniel Radcliffe",
+              "Emma Watson",
+              "Rupert Grint",
+              "Alan Rickman",
+              "Helena Bonham Carter"
+          ]
+      },
+      {
+          "titulo": "Harry Potter e as Relíquias da Morte - Parte 2",
+          "ano_lancamento": 2011,
+          "atores_principais": [
+              "Daniel Radcliffe",
+              "Emma Watson",
+              "Rupert Grint",
+              "Alan Rickman",
+              "Maggie Smith"
+          ]
+      }
+  ]
+}
+
+module.exports = harryPotterMovies;
